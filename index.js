9383956d395ff737ec6e39dc0294c9b0932713da
Miguel Martin <mmruvinsky@gmail.com>
2023-02-22 14:38:07 -0300

    se agrega el proyecto

---
+++ index.js
@@ -0,0 +1,9 @@
+function mostrar(){
+    document.getElementById('contacto').style.display = "block";
+    }
+
+function ocultar(){
+    document.getElementById('contacto').style.display = "none";
+    }
+
+
